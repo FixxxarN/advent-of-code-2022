4e3efbe74a6d9481d86050b7131759d02a1b890e
FixxxarN <tornstromandre@gmail.com>
2022-12-06 06:37:51 +0100

    Day 6 - Added comments

--- day6/day6.js
+++ day6/day6.js
@@ -9,7 +9,7 @@ let result = 0;
 
 for(let i = 0; i < arr.length; i++) {
   lastDistinct.push(arr[i]);
-  if(lastDistinct.length > 4) { // <----- 4 for part 1 and 14 for part 2
+  if(lastDistinct.length > 14) { // <----- 4 for part 1 and 14 for part 2
     lastDistinct = [...lastDistinct.slice(1, lastDistinct.length)]
     if(new Set(lastDistinct).size === lastDistinct.length) {
       result = i + 1;
@@ -17,5 +17,9 @@ for(let i = 0; i < arr.length; i++) {
     }
   }
 }
+
+// Part 1 1850
+
+// Part 2 2823
 
 console.log(result);
